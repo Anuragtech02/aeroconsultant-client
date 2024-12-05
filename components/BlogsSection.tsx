95dc6d94542ba1c975a69e60ab2a7e79ffc72274
Anurag Pal <apal895@gmail.com>
2024-12-05 14:37:40 +0530

    added blogs sectino, not responsive yet

--- components/BlogsSection.tsx
+++ components/BlogsSection.tsx
@@ -1,26 +1,26 @@
 import React from "react";
-import { ArrowUpRight } from "lucide-react";
+import ArrowExternalIcon from "@/app/icons/ArrowExternalIcon";
 
 const BlogSection = () => {
   const blogPosts = [
     {
       id: 1,
-      image: "/blog-thumb1.jpg",
+      image: "plane-img.jpg",
       title:
         "Good afternoon. As we are due to meet next Tuesday afternoon, I thought it might be an idea to send over...",
     },
     {
       id: 2,
-      image: "/blog-thumb2.jpg",
+      image: "plane-img.jpg",
       title:
         "Good afternoon. As we are due to meet next Tuesday afternoon, I thought it might be an idea to send over...",
     },
   ];
 
   return (
-    <section className="w-full bg-white py-16">
+    <section className="w-full border-y border-black/30">
       <div className="max-w-7xl mx-auto relative">
-        <div className="grid grid-cols-12 h-[600px]">
+        <div className="grid grid-cols-12 h-[700px]">
           {/* Left Column - Featured Blog */}
           <div className="col-span-7 relative h-full">
             <div className="absolute top-0 right-0 h-full w-[calc(100%+((100vw-1280px)/2))] -left-[calc((100vw-1280px)/2)]">
@@ -34,7 +34,14 @@ const BlogSection = () => {
                 <h2 className="font-normal mb-4">
                   5 tips to digitalize your aircraft documents
                 </h2>
-                <p className="text-sm">Read full article at Aerotalks.com</p>
+                <a
+                  className="text-sm underline"
+                  href="https://aerotalks.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Read full article at Aerotalks.com
+                </a>
               </div>
             </div>
           </div>
@@ -48,8 +55,8 @@ const BlogSection = () => {
                   key={post.id}
                   className="relative h-1/2 flex items-center pl-16"
                 >
-                  <div className="absolute -left-8 top-1/2 -translate-y-1/2 w-16 h-16">
-                    <div className="absolute inset-0 rounded-full border-2 border-white bg-white overflow-hidden ring-4 ring-white">
+                  <div className="absolute -left-8 top-1/2 -translate-y-1/2 w-20 h-20">
+                    <div className="absolute inset-0 rounded-full border-2 border-white bg-white overflow-hidden ring-2 ring-white">
                       <img
                         src={post.image}
                         alt="Blog thumbnail"
@@ -59,7 +66,7 @@ const BlogSection = () => {
                   </div>
                   <div>
                     <p className="text-base text-gray-900">{post.title}</p>
-                    <button className="text-sm text-purple-600 mt-1">
+                    <button className="text-sm mt-1 underline">
                       read more
                     </button>
                   </div>
@@ -69,12 +76,14 @@ const BlogSection = () => {
 
             {/* AeroTalks Section - Exactly half height */}
             <div className="relative h-1/2">
-              <div className="absolute inset-0 right-[calc(-1*(100vw-1280px)/2)] bg-purple-600 text-white p-8">
-                <div className="flex items-start justify-between">
-                  <h3 className="text-3xl font-bold">AeroTalks</h3>
-                  <ArrowUpRight className="w-6 h-6" />
+              <div className="absolute inset-0 right-[calc(-1*(100vw-1280px)/2)] bg-purple-600 text-white p-8 flex justify-between items-end">
+                <div>
+                  <span className="font-bold block text-6xl font-proxima">
+                    AeroTalks
+                  </span>
+                  <p className="text-lg mt-2">Read latest blogs & news</p>
                 </div>
-                <p className="text-lg mt-2">Read latest blogs & news</p>
+                <ArrowExternalIcon />
               </div>
             </div>
           </div>
